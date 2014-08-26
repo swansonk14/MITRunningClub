dc5c281efc78e3561b5310f4eda64e6002422734
Ken Leidal <kkleidal@gmail.com>
2014-08-26 13:38:22 -0400

    added sample config file

---
+++ config.sample.php
@@ -0,0 +1,13 @@
+<?php
+
+define( "FACEBOOK_SDK_LOCATION", __DIR__ . "/libs/facebook-php-sdk-v4-4.0-dev/autoload.php" );
+define( "FACEBOOK_APP_ID", "YOUR APP ID" );
+define( "FACEBOOK_APP_SECRET", "YOUR APP CERTIFICATE" );
+define( "FACEBOOK_PUBLIC_ACCESS_TOKEN", "YOUR PUBLIC ACCESS TOKEN" );
+
+define( "FACEBOOK_GROUP_ID", "615937005130415" );
+define( "NUMBER_PER_PAGE_RESULTS", 5 );
+
+$SHOW_POSTS_FROM = Array( "MEMBER'S FACEBOOK ID NUMBER" => true );
+
+?>
